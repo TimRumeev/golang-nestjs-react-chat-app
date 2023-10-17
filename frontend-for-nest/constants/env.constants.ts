6429513082a4e36a96cf91e3ce3eaf5ae7288248
TimRumeev <trumeev@mail.com>
2023-10-17 19:14:26 +0500

    asdasdasdasdsadasdas`1111

--- frontend-for-nest/constants/env.constants.ts
+++
@@ -1,7 +0,0 @@
-const env = {
-  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:3001",
-  SOCKET_SERVER_URL:
-    process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:3002",
-};
-
-export default env;
